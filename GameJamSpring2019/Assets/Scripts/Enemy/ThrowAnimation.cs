bccc29e308241ed68e5569e82bb75a57f691e5b4
010110 <joannedaeunjung@gmail.com>
2019-06-01 16:15:27 -0400

    Coal Person
    Weiiird

---
+++ GameJamSpring2019/Assets/Scripts/Enemy/ThrowAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+using UnityEditor.Animations; 
+
+public class ThrowAnimation : MonoBehaviour
+{
+    Animator anim;
+    int pr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
