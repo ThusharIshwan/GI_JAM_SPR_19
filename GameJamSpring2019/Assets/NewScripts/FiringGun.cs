cb9605989d6c4e8bc88d45eea57988c8c5d92a2c
Hartsie Patches <33205355+ThusharIshwan@users.noreply.github.com>
2019-06-02 16:49:06 -0400

    as far as this repo will go
    Mission failed. We'll get'em next time.

---
+++ GameJamSpring2019/Assets/NewScripts/FiringGun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringGun : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject barrel;
+    //private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fire()
+    {
+        Instantiate(bullet, barrel.GetComponent<Transform>());
+    }
+
+
+}
---
+++ GameJamSpring2019/Assets/NewScripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Transform pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.position = new Vector3(pos.position.x + (Input.GetAxis("Horizontal")* speed), pos.position.y, pos.position.z + (Input.GetAxis("Vertical") * speed));
+    }
+}
