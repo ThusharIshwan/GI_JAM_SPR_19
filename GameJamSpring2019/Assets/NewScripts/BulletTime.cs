aeeb80433abc0a697bd3f6f1224cb52ad28ca151
ThusharIshwan <33205355+ThusharIshwan@users.noreply.github.com>
2019-06-02 10:56:36 -0400

    Gun progress
    Things to add:
    
    Trigger
    EnemyHit

---
+++ GameJamSpring2019/Assets/NewScripts/BulletTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int timer;
+    public GameObject barrel;
+    public float shotforce;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<Transform>().position = barrel.GetComponent<Transform>().position;
+        this.gameObject.GetComponent<Transform>().rotation = barrel.GetComponent<Transform>().rotation;
+        this.gameObject.GetComponent<Rigidbody>().AddForce(shotforce * Vector3.right);
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
